3251edc946fe146861d26eb1f751626cbea5d0c8
AudunSorheim <audun.sorheim@nav.no>
2023-09-19 14:48:20 +0200

    Delete cypress test

--- src/App.cy.tsx
+++ src/App.cy.tsx
@@ -1,10 +1,5 @@
 import React from "react";
 
 describe("<App />", () => {
-  // it("Displays brev of type INNKALLING", () => {
-  //   cy.mountWithStubs(<App />, { dialogmoteResponse: [createInnkallingsBrev()] });
-  //
-  //   cy.contains("Dialogmøte med NAV");
-  //   cy.contains("Du har ikke svart");
-  // });
+  //   ...
 });
