574b979acfc356b459a055e890e9c45c32adfa9b
AudunSorheim <audun.sorheim@nav.no>
2023-10-03 11:45:16 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,44 @@
 import React from "react";
 import { ForhaandsvarselPanel } from "./components/panels/ForhaandsvarselPanel";
-import { Fetcher } from "swr";
-import { AktivitetskravVurdering } from "./schema/aktivitetskravVurderingSchema";
-import useSWRImmutable from "swr/immutable";
-import { aktivitetskravApiUrl } from "./api/urls";
-import { UnderArbeidPanel } from "./components/panels/UnderArbeidPanel";
-import { UnntakPanel } from "./components/panels/UnntakPanel";
-import { OppfyltPanel } from "./components/panels/OppfyltPanel";
-import { IkkeAktuellPanel } from "./components/panels/IkkeAktuellPanel";
-import { get } from "./api/api";
+import { subtractDaysFromDate } from "./utils/dateUtils";
 
 function App() {
-  const fetchAktivitetskravVurdering: Fetcher<AktivitetskravVurdering, string> = (path) => get(path);
-  const aktivitetskravResponse = useSWRImmutable(aktivitetskravApiUrl, fetchAktivitetskravVurdering);
+  return <ForhaandsvarselPanel journalpostId="123" fristDato={subtractDaysFromDate(new Date(), 14).toISOString()} />;
 
-  if (aktivitetskravResponse.data) {
-    switch (aktivitetskravResponse.data.status) {
-      case "NY":
-        return <UnderArbeidPanel />;
-      case "UNNTAK":
-        return (
-          <UnntakPanel
-            arsak={aktivitetskravResponse.data.arsaker}
-            sistVurdert={aktivitetskravResponse.data.sistVurdert}
-          />
-        );
-      case "OPPFYLT":
-        return (
-          <OppfyltPanel
-            arsak={aktivitetskravResponse.data.arsaker}
-            sistVurdert={aktivitetskravResponse.data.sistVurdert}
-          />
-        );
-      case "FORHANDSVARSEL":
-        return (
-          <ForhaandsvarselPanel
-            journalpostId={aktivitetskravResponse.data.journalpostId}
-            fristDato={aktivitetskravResponse.data.fristDato}
-          />
-        );
-      case "IKKE_AKTUELL":
-        return <IkkeAktuellPanel sistVurdert={aktivitetskravResponse.data.sistVurdert} />;
-    }
-  }
-
-  return null;
+  // const fetchAktivitetskravVurdering: Fetcher<AktivitetskravVurdering, string> = (path) => get(path);
+  // const aktivitetskravResponse = useSWRImmutable(aktivitetskravApiUrl, fetchAktivitetskravVurdering);
+  //
+  // if (aktivitetskravResponse.data) {
+  //   switch (aktivitetskravResponse.data.status) {
+  //     case "NY":
+  //       return <UnderArbeidPanel />;
+  //     case "UNNTAK":
+  //       return (
+  //         <UnntakPanel
+  //           arsak={aktivitetskravResponse.data.arsaker}
+  //           sistVurdert={aktivitetskravResponse.data.sistVurdert}
+  //         />
+  //       );
+  //     case "OPPFYLT":
+  //       return (
+  //         <OppfyltPanel
+  //           arsak={aktivitetskravResponse.data.arsaker}
+  //           sistVurdert={aktivitetskravResponse.data.sistVurdert}
+  //         />
+  //       );
+  //     case "FORHANDSVARSEL":
+  //       return (
+  //         <ForhaandsvarselPanel
+  //           journalpostId={aktivitetskravResponse.data.journalpostId}
+  //           fristDato={aktivitetskravResponse.data.fristDato}
+  //         />
+  //       );
+  //     case "IKKE_AKTUELL":
+  //       return <IkkeAktuellPanel sistVurdert={aktivitetskravResponse.data.sistVurdert} />;
+  //   }
+  // }
+  //
+  // return null;
 }
 
 export default App;
