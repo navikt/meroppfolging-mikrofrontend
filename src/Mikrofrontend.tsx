dc29c9506351e3147ec3a6aa59210e62766cdc1c
AudunSorheim <audun.sorheim@nav.no>
2023-11-08 08:33:09 +0100

    Properly hide mikrofrontend

--- src/Mikrofrontend.tsx
+++ src/Mikrofrontend.tsx
@@ -1,22 +1,12 @@
 import React from "react";
 import App from "./App";
-import styled from "styled-components";
 import { ErrorBoundary } from "react-error-boundary";
 import { logError } from "./faro/faro";
-
-const Wrapper = styled.div`
-  width: 100%;
-  @media (min-width: 648px) {
-    max-width: 444px;
-  }
-`;
 
 const Mikrofrontend = () => {
   return (
     <ErrorBoundary fallback={<></>} onError={logError}>
-      <Wrapper>
-        <App />
-      </Wrapper>
+      <App />
     </ErrorBoundary>
   );
 };
