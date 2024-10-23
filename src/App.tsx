00aaac875fa4da523b7709234e4a70e9f8070505
Håkon Møller <hakon.salomonsen.moller@nav.no>
2024-10-23 11:25:56 +0200

    Text changes
    https://trello.com/c/p1S8fxD1/1336-tekstendringer-p%C3%A5-oppf%C3%B8lging-sen-fase

--- src/App.tsx
+++ src/App.tsx
@@ -21,14 +21,14 @@ function App() {
     if (data.responseStatus == "NO_RESPONSE") {
       const maxDateText =
         data.maxDate != null
-          ? `Den ${data.maxDate} er datoen du ikke lenger vil motta sykepenger (maksdato).`
-          : `Det nærmer seg datoen du ikke lenger vil motta sykepenger.`;
+          ? `Siste dag du kan motta sykepenger er beregnet til ${data.maxDate}.`
+          : `Det nærmer seg siste dag du kan motta sykepenger.`;
 
       return (
         <MikrofrontendLinkPanel
           headingText="Snart slutt på sykepengene"
-          bodyText={`${maxDateText} Vi trenger derfor at du svarer på noen få spørsmål om situasjonen du står i så vi kan hjelpe deg.`}
-          alertStyle="warning"
+          bodyText={`${maxDateText} Vi ber derfor om at du svarer på om du ønsker å snakke med en veileder.`}
+          alertStyle="info"
           tag={{ variant: "warning-moderate", text: "Du har ikke svart" }}
         />
       );
@@ -50,7 +50,7 @@ function App() {
           return (
             <MikrofrontendLinkPanel
               headingText="Snart slutt på sykepengene"
-              bodyText="Du har svart at du har behov for hjelp fra NAV. En veileder vil ta kontakt med deg."
+              bodyText="Du har svart at du ønsker å snakke med en veileder. Vi tar kontakt med deg."
               alertStyle="info"
               tag={{ variant: "success-moderate", text: `Du svarte den ${responseDate.format(datePattern)}` }}
             />
@@ -59,7 +59,7 @@ function App() {
           return (
             <MikrofrontendLinkPanel
               headingText="Snart slutt på sykepengene"
-              bodyText="Du har svart at du ikke har behov for hjelp fra NAV. Ta kontakt hvis situasjonen din endrer seg."
+              bodyText="Du har svart at du ikke trenger å snakke med en veileder nå. Ta kontakt hvis situasjonen din endrer seg."
               alertStyle="info"
               tag={{ variant: "success-moderate", text: `Du svarte den  ${responseDate.format(datePattern)}` }}
             />
